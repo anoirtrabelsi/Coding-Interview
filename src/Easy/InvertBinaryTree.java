d6e6cbb661d80d37dc771c51929e8cc7c95d2324
Anoir Trabelsi <anoir.trabelsi@tum.de>
2019-09-21 17:01:04 +0200

    New Problems

--- src/Easy/InvertBinaryTree.java
+++ src/Easy/InvertBinaryTree.java
@@ -1,8 +1,5 @@
 package Easy;
 
-import java.util.*;
-import java.util.Arrays;
-
 public class InvertBinaryTree {
 
 	// Invert Binary Tree: https://leetcode.com/problems/invert-binary-tree/
@@ -30,60 +27,6 @@ public class InvertBinaryTree {
 		root.right = invertTree(tempLeft);
 
 		return root;
-	}
-
-	public int maxNumberOfApples(int[] arr) {
-
-		Arrays.sort(arr);
-
-		int sum = 0;
-
-		for (int i = 0; i < arr.length; i++) {
-			sum += arr[i];
-		}
-		int res = arr.length;
-
-		if (sum <= 5000)
-			return res;
-		int index = arr.length - 1;
-		while (sum > 5000) {
-			sum -= arr[index--];
-			res--;
-		}
-		return res;
-	}
-
-	public int smallestCommonElement(int[][] mat) {
-
-		if (mat.length == 1)
-			return mat[0][0];
-
-		Set<Set<Integer>> set = new HashSet<Set<Integer>>();
-
-		for (int i = 1; i < mat.length; i++) {
-			Set<Integer> curr = new HashSet<Integer>();
-			for (int x : mat[i])
-				curr.add(x);
-			set.add(curr);
-		}
-
-		int[] arr = mat[0];
-
-		Arrays.sort(arr);
-
-		for (int i = 0; i < arr.length; i++) {
-			int num = 0;
-			for (Set<Integer> s : set) {
-				if (!s.contains(arr[i]))
-					break;
-				else
-					num++;
-				if (num == set.size())
-					return arr[i];
-			}
-		}
-
-		return -1;
 	}
 
 }
